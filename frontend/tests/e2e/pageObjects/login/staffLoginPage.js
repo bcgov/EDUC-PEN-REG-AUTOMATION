5ec575dced7c9acbb5fd8f6862f9a438d94b8a15
revanthreddygandra <revanth.reddy@gov.bc.ca>
2021-03-23 13:20:07 -0700

    staff login

--- frontend/tests/e2e/pageObjects/login/staffLoginPage.js
+++ frontend/tests/e2e/pageObjects/login/staffLoginPage.js
@@ -19,25 +19,22 @@ class staffLoginPage {
 
     async stafflogin(credentials) {
 
-        try {
-            await t.click(this.login)
-                .typeText(this.username, credentials.username, { timeout: 20000 })
-                .typeText(this.password, credentials.password, { timeout: 20000 })
-                .click(this.submitButton)
-            await t.expect((this.viewGmpButton).exists).ok()
-            log.info("Staff user successfully logged in with IDIR    " + credentials.username)
-        }
-        catch (err) {
-            await t.eval(() => location.reload())
-            log.warn("Element not found, Refreshing the page")
-            await t
-                .typeText(this.username, credentials.username, { timeout: 20000 })
-                .typeText(this.password, credentials.password, { timeout: 20000 })
-                .click(this.submitButton)
-            await t.expect((this.viewGmpButton).exists).ok()
-            log.info("Staff user successfully logged in with IDIR    " + credentials.username)
-            // const getURL = await ClientFunction(() => window.location.href)();
-            // console.log(getURL)
+        for (let i = 0; i < 10; i++) {
+            try {
+                await t.click(this.login)
+                    .typeText(this.username, credentials.username, { timeout: 20000 })
+                    .typeText(this.password, credentials.password, { timeout: 20000 })
+                    .click(this.submitButton)
+                await t.expect((this.viewGmpButton).exists).ok()
+                log.info("Staff user successfully logged in with IDIR    " + credentials.username)
+                break
+            }
+            catch (err) {
+                await t.eval(() => location.reload())
+                log.warn("Element not found, Refreshing the page")
+                // const getURL = await ClientFunction(() => window.location.href)();
+                // console.log(getURL)
+            }
         }
     }
 
