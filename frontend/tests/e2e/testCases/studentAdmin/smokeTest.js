248531d275daa485302877f1691fe49671d59641
revanthreddygandra <revanth.reddy@gov.bc.ca>
2021-08-10 12:13:23 -0700

    smoke tests

---
+++ frontend/tests/e2e/testCases/studentAdmin/smokeTest.js
@@ -0,0 +1,50 @@
+import staffLoginPage from '../../pageObjects/login/staffLoginPage';
+import staffDashboardPage from '../../pageObjects/studentAdmin/dashboard/staffDashboardPage'
+import staffPenRetrievalRequestPage from '../../pageObjects/studentAdmin/gmp/staffPenRetrievalRequestsPage'
+import { idirAdminCredentials, staffLoginUrl, penNumber } from '../../config/constants';
+import studentSearchResult from '../../config/studentData/studentSearchResults.json'
+import staffData from '../../config/staffData/staffData.json'
+import staffHamburgerMenuPage from '../../pageObjects/studentAdmin/dashboard/staffHamburgerMenuPage'
+import staffUmpRequestsPage from '../../pageObjects/studentAdmin/ump/staffUmpRequestsPage'
+import staffStudentSearchPage from '../../pageObjects/studentAdmin/studentSearch/staffStudentSearchPage'
+
+const staffLogin = new staffLoginPage()
+const penPage = new staffPenRetrievalRequestPage()
+const dashboard = new staffDashboardPage()
+const menu = new staffHamburgerMenuPage()
+const umpPage = new staffUmpRequestsPage()
+const staffSearch = new staffStudentSearchPage()
+
+fixture`Student Admin`
+    .page(staffLoginUrl)
+    .beforeEach(async t => {
+        await t.maximizeWindow()
+    })
+
+test('Staff login and issue pen to Student test', async t => {
+
+    await staffLogin.stafflogin(idirAdminCredentials, staffLoginUrl)
+
+    await dashboard.clickViewGmpButton()
+
+    await penPage.setMainStatusBar(staffData.Status[0])
+
+    await menu.clickHamburgerMenu()
+
+    await menu.clickDashboardLink()
+
+    await dashboard.clickViewUmpButton()
+
+    await umpPage.setMainStatusBar(staffData.Status[0])
+
+    await menu.clickHamburgerMenu()
+
+    await menu.clickDashboardLink()
+
+    await dashboard.clickFullSearchButton()
+
+    await staffSearch.setPen(penNumber)
+
+    await staffSearch.verifyStudentSearchResult(penNumber, studentSearchResult)
+
+});
---
+++ frontend/tests/e2e/testCases/studentProfile/smokeTest.js
@@ -0,0 +1,69 @@
+import studentLoginPage from '../../pageObjects/login/studentLoginPage';
+import penRequestForm from '../../pageObjects/studentProfile/gmp/penRequestForm';
+import constants from '../../config/constants';
+import studentData from '../../config/studentData/studentData.json';
+import updateMyPenInfoPage from '../../pageObjects/studentProfile/ump/updateMyPenInfoPage'
+
+
+const studentLogin = new studentLoginPage()
+const penRequest = new penRequestForm()
+const updatePage = new updateMyPenInfoPage()
+
+if (constants.studentEntryPoint == "landingPage") {
+  fixture`Student Profile`
+    .page(
+      constants.studentProfileUrl)
+    .beforeEach(async t => {
+      await t.maximizeWindow()
+    })
+}
+else if (constants.studentEntryPoint == "gmp") {
+  fixture`Student Profile`
+    .page(
+      constants.studentProfileUrlGmp)
+    .beforeEach(async t => {
+      await t.maximizeWindow()
+    })
+}
+
+test('Bceid Login and Fill Pen request form test', async t => {
+
+  await studentLogin.bceidLogin(constants.bceidCredentials , constants.studentEntryPoint , constants.studentProfileUrl, constants.studentProfileUrlGmp)
+
+  await studentLogin.overcomeAccountActivity()
+
+  await studentLogin.clickGetMyPen(constants.studentEntryPoint)
+
+  await studentLogin.overcomeCreateNewPenRequest()
+
+  await penRequest.fillRequestForm(studentData, false, constants.pen_environment);
+
+  await studentLogin.clickOnDisplayName()
+
+  await studentLogin.clickHomeButton()
+
+  await studentLogin.clickUpdateMyPen(constants.studentEntryPoint)
+
+  await updatePage.setPenNumber(constants.penNumber)
+
+  await updatePage.setLegalFirstName(studentData.legalFirstName)
+
+  await updatePage.setLegalLastName(studentData.legalLastName)
+
+  await updatePage.setBirthDate(studentData)
+
+  await updatePage.setGender(studentData)
+
+  await updatePage.clickNextButton()
+
+  await updatePage.clickIDeclareCheckBox()
+
+  await updatePage.editFirstName(studentData.legalLastName)
+
+  await updatePage.setEmail(studentData.umpEmail,constants.pen_environment)
+
+  await updatePage.clickAccurateCheckBox()
+
+  await updatePage.clickNextButton()
+
+});
