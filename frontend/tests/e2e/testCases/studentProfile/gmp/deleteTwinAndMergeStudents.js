9fe3cffa25f665cb5ba3b37cfab5c1a075ede637
revanthreddygandra <revanth.reddy@gov.bc.ca>
2021-08-13 11:39:02 -0700

    updates

--- frontend/tests/e2e/testCases/studentProfile/gmp/deleteTwinAndMergeStudents.js
+++ frontend/tests/e2e/testCases/studentProfile/gmp/deleteTwinAndMergeStudents.js
@@ -5,9 +5,10 @@ const log = require('npmlog')
 
 getToken().then(async (data) => {
 
-    
     for (let i = 0; i <= 5; i++) {
 
+        getToken().then((data))
+        
         const token = data.access_token
 
         let searchListCriteria = []
