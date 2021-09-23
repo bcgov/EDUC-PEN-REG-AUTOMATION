9232313544c929c5a5ff38bb904426599ade89dd
revanthreddygandra <revanth.reddy@gov.bc.ca>
2021-09-23 15:00:13 -0700

    disabled search test

--- frontend/tests/e2e/testSuites/gmpE2E/staffIssuePenToStudentSuite.js
+++ frontend/tests/e2e/testSuites/gmpE2E/staffIssuePenToStudentSuite.js
@@ -18,7 +18,7 @@ createTestCafe('localhost', 1337, 1338)
                
                 "tests/e2e/testCases/studentProfile/mailsac/mailsacActivatePenRequest.js",
 
-                "tests/e2e/testCases/studentAdmin/gmp/staffSearchPenRequestData.js",
+                //"tests/e2e/testCases/studentAdmin/gmp/staffSearchPenRequestData.js",
                 
                 "tests/e2e/testCases/studentAdmin/gmp/staffIssuePenToStudent.js"
 
