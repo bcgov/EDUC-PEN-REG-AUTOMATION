5cbf6cb86ed4c32c05e439ef662f319462085d7e
revanthreddygandra <revanth.reddy@gov.bc.ca>
2021-09-27 10:58:22 -0700

    added batch pipeline

--- frontend/tests/e2e/config/constants.js
+++ frontend/tests/e2e/config/constants.js
@@ -43,7 +43,7 @@ module.exports.studentProfileApiUrl = 'http://student-profile-api-master.' + mod
 module.exports.penMatchApiUrl = 'http://pen-match-api-master.' + module.exports.pen_namespace + '-' + module.exports.token_environment + '.svc.cluster.local:8080/api/v1/pen-match'
 module.exports.penWebUrl = config.get('PenWeb:url');
 module.exports.myEdUrl = 'http://pen-myed-api-master.' + module.exports.pen_namespace + '-' + module.exports.token_environment + '.svc.cluster.local:8080/',
-module.exports.penRegBatchUrl = 'https://pen-reg-batch-api-' + module.exports.pen_namespace + '-' + module.exports.token_environment + '.apps.silver.devops.gov.bc.ca/api/v1/pen-request-batch'
+module.exports.penRegBatchUrl = 'https://pen-reg-batch-api-' + module.exports.pen_namespace + '-' + module.exports.token_environment + '.svc.cluster.local:8080/api/v1/pen-request-batch'
 
 
 //Choose student entry point
