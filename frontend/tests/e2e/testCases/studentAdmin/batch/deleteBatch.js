3a17d7ea68a49541c4432b241bfefedadc011ca6
revanthreddygandra <revanth.reddy@gov.bc.ca>
2022-03-03 12:32:34 -0800

    delete batch updates

--- frontend/tests/e2e/testCases/studentAdmin/batch/deleteBatch.js
+++ frontend/tests/e2e/testCases/studentAdmin/batch/deleteBatch.js
@@ -8,7 +8,7 @@ getToken().then(async (data) => {
 
     const token = data.access_token;
 
-    for (let i = 0; i <= batchFiles.batchData.test.length - 1; i++) {
+    for (let i = 2; i <= batchFiles.batchData.test.length - 1; i++) {
 
         const submissionNumber = batchFiles.batchData.test[i].submissionNumber
 
