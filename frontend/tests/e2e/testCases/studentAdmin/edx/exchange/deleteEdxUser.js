2a6d7c3b7874a9c9d0ac3e901c22deb0e595e447
SoLetsDev <derek.so@gov.bc.ca>
2022-06-13 09:24:48 -0700

    created access management test with insert / delete scripts in DEV environment for EDX-15

---
+++ frontend/tests/e2e/testCases/studentAdmin/edx/exchange/deleteEdxUser.js
@@ -0,0 +1,28 @@
+const helper = require("../../../../helpers/axios-helper");
+const constants = require('../../../../config/constants');
+const { getToken } = require('../../../../helpers/generateToken');
+const log = require('npmlog');
+
+getToken().then(async (data) => {
+  const token = data.access_token
+
+  // get EDX Users at school
+  const edxUsers = await helper.getData(token, `${constants.EDXApiUrl}users`, {params: {mincode: "00899178"}});
+  log.info('edxUsers list obtained');
+
+  const edxUserID = edxUsers.find(edxUser => edxUser.firstName === "automation" && edxUser.lastName === "tester")?.edxUserID;
+
+  if (edxUserID) {
+    log.info('obtained user ID for tester');
+    await helper.deleteData(token, `${constants.EDXApiUrl}users/${edxUserID}`);
+    log.info("user deleted");
+  } else {
+    log.warn("user not found - did not delete");
+  }
+
+})
+
+.catch((error => {
+  console.log(error)
+  throw new Error("delete EDX user failed")
+}))
