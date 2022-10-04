f7936ff902b34abb2011ec7196f7853a1697615e
SoLetsDev <derek.so@gov.bc.ca>
2022-10-04 08:45:26 -0700

    formatting

--- frontend/tests/e2e/testCases/studentAdmin/keycloak/deleteBceidFromKC.js
+++ frontend/tests/e2e/testCases/studentAdmin/keycloak/deleteBceidFromKC.js
@@ -3,21 +3,15 @@ const constants = require('../../../config/constants')
 const {getToken} = require('../../../helpers/generateToken')
 
 getToken().then(async (data) => {
+  const token = data.access_token;
+  const kcResponse = await helper.getData(token,`${constants.kcUrl}?username=${constants.bceidCredentials.guid}@bceidbasic`);
+  const userId = kcResponse[0].id;
 
-      const token = data.access_token
+  const deleteUser = await helper.deleteData(token,`${constants.kcUrl}/${userId}`);
+  console.log(deleteUser);
+}).catch((error => {
+  console.log(error);
+  throw new Error("Test failed");
+}));
 
-      const kcResponse = await helper.getData(token,
-          `${constants.kcUrl}?username=${constants.bceidCredentials.guid}@bceidbasic`)
-
-      const userId = kcResponse[0].id
-
-      const deleteUser = await helper.deleteData(token,
-          `${constants.kcUrl}/${userId}`)
-      console.log(deleteUser)
-
-    }
-).catch((error => {
-  console.log(error)
-  throw new Error("Test failed")
-}))
 
