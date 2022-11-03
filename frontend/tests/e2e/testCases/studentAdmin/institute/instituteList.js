27530139d3e53959e2ee204b9a60d54a388b0621
arcshiftsolutions <arcshiftsolutions@gmail.com>
2022-11-03 10:35:29 -0700

    Setting screen resolution.

--- frontend/tests/e2e/testCases/studentAdmin/institute/instituteList.js
+++ frontend/tests/e2e/testCases/studentAdmin/institute/instituteList.js
@@ -15,7 +15,7 @@ const schoolsPage = new SchoolsPage();
 fixture`Student Admin`
   .page(staffLoginUrl)
   .beforeEach(async t => {
-    await t.maximizeWindow()
+    await t.resizeWindow(1920, 1080)
   });
 
 test('Staff search districts test', async () => {
